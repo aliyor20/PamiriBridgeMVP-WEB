78e979dc6c26ca976b17129772d138ca4b781b5f
Aliyor <aliyoresoev1@gmail.com>
2026-01-21 22:33:29 -0800

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
+import { PreferencesProvider } from './src/context/PreferencesContext';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <PreferencesProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </PreferencesProvider>
   );
 }
